e1e52f4450e89032ce606befe0c18c8daf3de9de
GiulianaCDA <giulianaarecippo@hotmail.com>
2022-04-02 16:21:18 -0300

    Altera formatação

--- Main.java
+++ Main.java
@@ -4,16 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        ArrayList<User> users = new ArrayList<User>();
-        boolean run = true;
-        boolean exists = false;
-        User current_user = null;
-
-        System.out.print("\n-----------------------------\n");
-        System.out.print("     BEM VINDO AO IFACE!     ");
-        System.out.print("\n-----------------------------\n");
+    public static void time(){
 
         try{
             Thread.sleep(1000);
@@ -21,6 +12,38 @@ public class Main {
         catch (Exception e) {
             System.out.println(e);
         }
+
+    }
+
+    public static User createUser(){
+
+        String name, name_user, password;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("\n\n-- CADASTRO DE USUÁRIO --\n");
+        System.out.print("\nDigite seu nome: ");
+        name = input.next();
+
+        System.out.print("Digite seu nome de usuário: ");
+        name_user = input.next();
+
+        System.out.print("Digite uma senha forte: ");
+        password = input.next();
+
+        System.out.print("\n- USUÁRIO CADASTRADO! -");
+
+        User new_user = new User(name, name_user, password);
+
+        return new_user;
+    }
+    public static void main(String[] args) {
+
+        ArrayList<User> users = new ArrayList<User>();
+        boolean run = true;
+        boolean exists = false;
+        User current_user = null;
+
+        System.out.print("\n----- BEM VINDO AO IFACE! -----\n");
 
         while(run){
             System.out.print("\n\n-- Escolha uma ação --\n");
@@ -30,36 +53,19 @@ public class Main {
             System.out.print("\n[4] Verificar dados\n");
             System.out.print("_________________________\n\n");
             System.out.print("Resposta [1-4]: ");
+
             Scanner input = new Scanner(System.in);
             int option = input.nextInt();
     
             if(option == 1) {
+
                 exists = true;
-                String name, name_user, password;
-    
-                System.out.print("\n\n-- CADASTRO DE USUÁRIO --\n");
-                System.out.print("\nDigite seu nome: ");
-                name = input.next();
-        
-                System.out.print("Digite seu nome de usuário: ");
-                name_user = input.next();
-        
-                System.out.print("Digite uma senha forte: ");
-                password = input.next();
-    
-                System.out.print("\n- USUÁRIO CADASTRADO! -");
+                User new_user = createUser();
 
-                User new_user = new User(name, name_user, password);
                 users.add(new_user);
-
                 current_user = new_user;
 
-                try{
-                    Thread.sleep(1000);
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
+                time();
 
             }
 
@@ -97,12 +103,7 @@ public class Main {
 
                 System.out.print("\n- EDIÇÃO REALIZADA! -");
 
-                try{
-                    Thread.sleep(1000);
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
+                time();
             }
 
             else if(option == 4){
